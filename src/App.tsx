fea8ee58c29afe9d4afbdb16eee183b0e9fca2a8
neyaznafiz <niiyazobug@gmail.com>
2022-10-11 16:37:09 +0600

    list and focuse component created

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,17 @@
 import React from "react";
-
+import { Route, Routes } from "react-router-dom";
+import Focus from "./Pages/Focus";
+import List from "./Pages/List";
 
 function App() {
-  return <div></div>;
+  return (
+    <div>
+      <Routes>
+        <Route path="/" element={<List/>} />
+        <Route path="/focus" element={<Focus/>} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
