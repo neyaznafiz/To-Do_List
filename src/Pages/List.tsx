9d0f78c72759397142b118885575cff3ac46b59f
neyaznafiz <niiyazobug@gmail.com>
2022-10-12 12:31:31 +0600

    complete taks clear function and after add input will be empty bug fixed

--- src/Pages/List.tsx
+++ src/Pages/List.tsx
@@ -4,38 +4,74 @@ import React, { ChangeEvent, KeyboardEvent, useState } from "react";
 // type
 type Props = {};
 
-type Tasks = {
+type Task = {
   id: string;
   label: string;
+  isComplete: boolean;
 };
 
 const List: React.FC<Props> = () => {
-  const [tasks, setTasks] = useState<Tasks[]>([]);
+  const [tasks, setTasks] = useState<Task[]>([]);
   const [newTaskLabel, setNewTaskLabel] = useState("");
 
+  //
   const handleNewTasksLableChange = (e: ChangeEvent<HTMLInputElement>) => {
     setNewTaskLabel(e.target.value);
   };
 
+  //
   const handleNewTakskKeyUp = (e: KeyboardEvent<HTMLInputElement>) => {
-    if (e.key === "Enter" && newTaskLabel !== '') {
-      setTasks((tasks) => [...tasks, { id: nanoid(), label: newTaskLabel }]);
+    if (e.key === "Enter" && newTaskLabel !== "") {
+      setTasks((tasks) => [
+        ...tasks,
+        {
+          id: nanoid(),
+          label: newTaskLabel,
+          isComplete: false,
+        },
+      ]);
     }
   };
 
+  //
+  const handleCompleteChange =
+    (handleTask: Task) => (e: ChangeEvent<HTMLInputElement>) => {
+      setTasks((tasks) =>
+        tasks.map((task) => {
+          if (task.id === handleTask.id)
+            return { ...task, isComplete: e.target.checked };
+          return task;
+        })
+      );
+    };
+
+  const handleClearClick = () =>
+    setTasks((tasks) => tasks.filter((task) => !task.isComplete));
+
   return (
     <div>
-      <ul>
+      <div>
         {tasks.map((task) => (
-          <li key={task.id}>{task.label}</li>
+          <div key={task.id}>
+            <input
+              type="checkbox"
+              checked={task.isComplete}
+              onChange={handleCompleteChange(task)}
+            />
+            {task.label}
+          </div>
         ))}
-      </ul>
+      </div>
 
       <input
         value={newTaskLabel}
         onChange={handleNewTasksLableChange}
         onKeyUp={handleNewTakskKeyUp}
       />
+
+      <div>
+        <button onClick={handleClearClick}>Clear Completed</button>
+      </div>
     </div>
   );
 };
