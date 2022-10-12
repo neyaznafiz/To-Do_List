a77bf1ab433a8fa9dacd320df767e820786ffd9a
neyaznafiz <niiyazobug@gmail.com>
2022-10-12 19:26:01 +0600

    navlik created

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,39 @@
 import React from "react";
-import { Route, Routes } from "react-router-dom";
+import { Route, Routes, NavLink } from "react-router-dom";
 import Focus from "./Pages/Focus";
 import List from "./Pages/List";
+import useState from 'react';
 
 function App() {
+  let activeStyle = {
+    fontWeight: "bold",
+  };
+
+  // const [tasks, setTasks] = useState<Task[]>([]);
+
   return (
     <div>
+      <div style={{ margin: "10px" }}>
+        <nav>
+          <NavLink
+            to="/"
+            style={({ isActive }) => (isActive ? activeStyle : undefined)}
+          >
+            List
+          </NavLink>{" "}
+          {""}- {""}
+          <NavLink
+            to="/focus"
+            style={({ isActive }) => (isActive ? activeStyle : undefined)}
+          >
+            Focus
+          </NavLink>
+        </nav>
+      </div>
+
       <Routes>
-        <Route path="/" element={<List/>} />
-        <Route path="/focus" element={<Focus/>} />
+        <Route path="/" element={<List />} />
+        <Route path="/focus" element={<Focus />} />
       </Routes>
     </div>
   );
